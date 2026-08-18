d33e35ea83ff8253553bf49d6dbc0211406353d6
heanvoo <jakutija1977@gmail.com>
2026-08-19 01:49:13 +0900

    blablabla

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ dp = Dispatcher()
 
 @dp.chat_join_request()
 async def approve_request(req: ChatJoinRequest):
+    # Принимаем заявку в канал
     await req.approve()
+    
+    # Отправляем сообщение пользователю в ЛС
+    try:
+        await bot.send_message(
+            chat_id=req.from_user.id,
+            text="Заявка принята, удачного использования Ngasaki Visuals!"
+        )
+    except Exception:
+        # Исключение сработает, если пользователь заблокировал бота или никогда не запускал его
+        pass
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
